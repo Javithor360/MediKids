bbb3521546f65d71109cd0fb545974b68746e05e
Javithor360 <floresmejia004@gmail.com>
2023-04-10 17:06:00 -0600

    [WEB] Implementación de Splash Welcome Page

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,29 +1,39 @@
 import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 
-import { HomePage, DownloadApp, DrFlores, Doc1, Doc2, Doc3, DraGarza, DrGuzman } from './pages'
+import {
+  HomePage,
+  DownloadApp,
+  DrFlores,
+  Doc1,
+  Doc2,
+  Doc3,
+  DraGarza,
+  DrGuzman,
+} from "./pages";
 import { ScrollToTop } from "./components";
+import withSplashScreen from "./components/withSplashScreen";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-          <ScrollToTop />
-        	<Routes>
-          		<Route>
-            	    <Route path="/" element={<Navigate to="/index" replace />} />
-                  <Route path="/index" element={<HomePage />} />
-                  <Route path="/download-app" element={<DownloadApp />} />
-                  <Route path="/otorrinolaringologo" element={<Doc1 />} />
-                  <Route path="/gastroenterología" element={<Doc2 />} />
-                  <Route path="/neumologia" element={<Doc3 />} />
-                  <Route path="/dr-flores" element={<DrFlores />} />
-                  <Route path="/dra-garza" element={<DraGarza />} />
-                  <Route path="/dr-guzman" element={<DrGuzman />} />
-          		</Route>
-        	</Routes>
+        <ScrollToTop />
+        <Routes>
+          <Route>
+            <Route path="/" element={<Navigate to="/index" replace />} />
+            <Route path="/index" element={<HomePage />} />
+            <Route path="/download-app" element={<DownloadApp />} />
+            <Route path="/otorrinolaringologo" element={<Doc1 />} />
+            <Route path="/gastroenterología" element={<Doc2 />} />
+            <Route path="/neumologia" element={<Doc3 />} />
+            <Route path="/dr-flores" element={<DrFlores />} />
+            <Route path="/dra-garza" element={<DraGarza />} />
+            <Route path="/dr-guzman" element={<DrGuzman />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </>
   );
 }
 
-export default App;
+export default withSplashScreen(App);
