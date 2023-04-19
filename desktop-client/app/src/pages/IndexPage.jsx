9b9d3b874dcd73202b276dc72844b45cacb971b8
Javithor360 <39425932+Javithor360@users.noreply.github.com>
2023-04-19 11:56:21 -0600

    [DESKTOP] Primeros avances en vista de pacientes activos

--- desktop-client/app/src/pages/IndexPage.jsx
+++ desktop-client/app/src/pages/IndexPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Modal from "../components/Modal.jsx";
 import { ModalTestComponent } from "../components/ModalTestComponent.jsx";
 
