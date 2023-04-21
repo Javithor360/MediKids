7d39248ded7849615a4087e0c918673b473c348a
DanielVasquezV <86084866+DanielVasquezV@users.noreply.github.com>
2023-04-20 20:47:02 -0600

    [DESKTOP] Avances en la interfaz de la pagina de inicio

--- desktop-client/app/src/components/Breadcrumbs.jsx
+++ desktop-client/app/src/components/Breadcrumbs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { RouteTranslator } from "../assets/js/RouteTranslator";
+import "./assets/scss/breadcrums.css"
 
 export const Breadcrumbs = () => {
   const location = useLocation();
@@ -19,14 +20,17 @@ export const Breadcrumbs = () => {
     .map((crumb) => {
       currentLink += `/${crumb}`;
       return (
-        <Link
-          className="after:inline-block text-[#a375ff] select-none hover:underline after:content-['/'] after:text-[#A2A2A2] after:ml-[0.25rem] after:mr-[0.25rem] last:after:collapse last:font-semibold"
-          to={currentLink}
-          key={crumb}
-        >
-          {assign(crumb.charAt(0).toUpperCase() + crumb.slice(1))}
-        </Link>
+        <>
+            <Link
+              className="after:inline-block text-[#a375ff] select-none hover:underline after:content-['/'] after:text-[#A2A2A2] after:ml-[0.25rem] after:mr-[0.05rem] last:after:hidden last:font-semibold right-0"
+              to={currentLink}
+              key={crumb}
+            >
+              {assign(crumb.charAt(0).toUpperCase() + crumb.slice(1))}
+            </Link>
+        </>
+        
       );
     });
-  return <div>{crumbs}</div>;
+    return <div className="lol h-[1.6rem] flex justify-end">{crumbs}</div>;
 };
--- desktop-client/app/src/components/Layout.jsx
+++ desktop-client/app/src/components/Layout.jsx
@@ -8,11 +8,12 @@ export const Layout = () => {
     <>
       <div className="flex overflow-hidden bg-[#e6e3e3]">
         <Sidebar />
-        <div className="w-full mx-6 min-h-[100%] h-[100%] relative">
-          <div className="flex justify-end m-5">
-            <Breadcrumbs />
+        <div className="w-full mx-5 min-h-[100%] h-[100%] relative">
+          <div className="lol1 flex justify-end items-center my-2 mx-5 h-[3rem] relative">
+            <div className="w-5 h-fit mr-3"><div className="w-full h-[2px] bg-[#A375FF]"></div></div>
+            <Breadcrumbs className=""/>
           </div>
-          <div className="w-full h-[75vh] bg-white overflow-y-auto overflow-x-hidden absolute p-[2rem] rounded-2xl">
+          <div className="w-full h-[85vh] bg-white overflow-y-auto overflow-x-hidden absolute p-[2rem] rounded-2xl">
             <Outlet />
           </div>
         </div>
