1b28c1b5c2596c70e585ce2ff29eaefda6f9d1f4
ELVISRJ <81792600+ElvisJua@users.noreply.github.com>
2023-04-25 06:32:02 -0600

    actualización del main
    home page

--- mobile-client/Screens/Home.js
+++ mobile-client/Screens/Home.js
@@ -5,13 +5,15 @@ const Home = () => {
   return (
     <View  style={styles.container}>
       <StatusBar  style="auto" />
- 
+ <View style={styles.contWave} >
       <Image
             style={styles.waves}
           source={require("../assets/wave-top-home-page.png")}
           
+          
         /> 
-       
+        
+       </View>
       
       
       <View style={styles.contLogo} >
@@ -19,19 +21,28 @@ const Home = () => {
           style={styles.Image}
           source={require("../assets/Logotype_Colored.png")}
         /> 
+      
  </View>
+
  <View style= {styles.bell}  >
+  <TouchableOpacity>
         <Image
- 
+   style= {styles.bell1}
       source={require("../assets/bell.png")}
       />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.points}>
+    <Text  style={styles.pointsT}   >...</Text> 
+    </TouchableOpacity>
         </View>
     
-       
+       <Text style={styles.saludo}>Hola</Text>
+       <Text style={styles.User}>Skyler white</Text>
+       <Text style={styles.Rol}>Encargado</Text>
      <View style={styles.card}  >
       <View style={styles.TextCard}>
     <Text style={styles.TextP1} >Nombre del paciente:</Text>
-
+    
 
     <Text style={styles.TextP2} >Edad:</Text>
     <Text style={styles.TextP3} >Género:</Text>
@@ -50,29 +61,71 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
 
   },
+ User:{
+  fontSize:20,
+marginTop:'-50%',
+marginBottom:'15%',
+marginLeft:'5%',
+ },
+ pointsT:{
+  fontSize:50,
+  color:'gray',
+ },
+points:{
+  marginLeft:'85%',
+  position:'absolute',
+  marginTop:'-5.5%',
+ 
+  
+},
+
   bell:{
     flexDirection:'row',
-    
+   marginTop:'-107%',
+   marginLeft:'6%',
+
   },
   
   contLogo:{
-    justifyContent: 'center',
-    alignItems: 'center',
-   
+    width: '66%',
+    height:'10%',
+
+    marginTop:'100%',
+ 
   },
   cont2:{
     flexDirection:'row',
     
+  },
+  contWave:{
+    position:'absolute',
+    marginTop:'0%',
+    marginBottom:'0%',
+    width: '100%',
+     height:'90%',
+  },
+  saludo:{
+fontSize:40,
+marginBottom:'50%',
+marginTop:'15%',
+marginLeft:'5%',
+  },
+  Rol:{
+    fontSize:20,
+    marginBottom:'50%',
+    marginTop:'-25%',
+    marginLeft:'75%',
+    color:'#d58c8c',
   },
 //Paciente
  card:{
   width:'80%',
   backgroundColor:'#A375FF',
 
-   
+   marginTop:'-40%',
       borderRadius:20,
       borderStyle: 'solid',
-      padding:'4%',
+      padding:'10%',
   margin:'-15%',
 marginLeft:'10%',
    
@@ -81,22 +134,34 @@ marginLeft:'10%',
     flexDirection:'row',
     gap:1,
     margin:'10%',
+    marginTop:'-10%',
 
   },
   TextP1:{
 margin:'10%',
+marginLeft:'-20%',
+color:'white',
+fontWeight:'bold',
+  },
+  TextN:{
+    color:'white',
   },
   TextP2:{
     margin:'10%',
+    color:'white',
+    fontWeight:'bold',
   },
   TextP3:{
     margin:'10%',
+    color:'white',
+    fontWeight:'bold',
   },
  //Fin Paciente 
   Image: {
-    width: '54.8%',
-  height:'41.4%',
-marginTop:'-65%',
+    width: '75%',
+  height:'44%',
+marginTop:'-130%',
+marginLeft:'40%',
 
   },
   
@@ -105,8 +170,8 @@ marginTop:'-65%',
      waves:{
       width: '100%',
      height:'20%',
+    
 
-     
      },
 
 });
