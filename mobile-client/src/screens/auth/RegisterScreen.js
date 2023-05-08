efde64aab8f150ecf9bf2443f5128e6301813621
Alvin Meléndez <86381337+RealAlvinJS@users.noreply.github.com>
2023-05-08 11:51:55 -0600

    [SERVER] LOLNT

--- mobile-client/src/screens/auth/RegisterScreen.js
+++ mobile-client/src/screens/auth/RegisterScreen.js
@@ -2,6 +2,8 @@
 import { Text, View, Image, TextInput, Dimensions, TouchableOpacity, ScrollView, KeyboardAvoidingView, ImageBackground} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
+
 //>> Importing components
 import  { AuthStylesGlobal, AuthStylesRegisterU }  from '../../../assets/AuthStyles';
 import { isAN, isIOS } from '../../constants';
@@ -9,6 +11,20 @@ import { CustomButton } from '../../index';
 
 export const RegisterScreen = () => {
   const navigation = useNavigation()
+
+  //! States for the Form.
+  const [FirstNames, setFirstNames] = useState(null);
+  const [LastNames, setLastNames] = useState(null);
+  const [DUI, setDUI] = useState(null);
+  const [Phone, setPhone] = useState(null);
+  const [Email, setEmail] = useState(null);
+  const [Password, setPassword] = useState(null);
+  const [ConfPass, setConfPass] = useState(null);
+
+  //! States for th functioning handler.
+  const [Error, setError] = useState(null);
+  const [Success, setSuccess] = useState(null);
+  
   return (
     <>
       <View style={AuthStylesGlobal.mainContainer}>
@@ -29,11 +45,13 @@ export const RegisterScreen = () => {
                 style={[AuthStylesGlobal.input, AuthStylesGlobal.customW50]}
                 placeholder="Nombres"
                 placeholderTextColor="gray"
+                onChangeText={text => setFirstNames(text)}
               />
               <TextInput
                 style={[AuthStylesGlobal.input, AuthStylesGlobal.customW50]}
                 placeholder="Apellidos"
                 placeholderTextColor="gray"
+                onChangeText={text => setLastNames(text)}
               />
             </View>
             <View style={{flexDirection: 'row', width: '90%', gap: 15}}>
@@ -41,17 +59,20 @@ export const RegisterScreen = () => {
                 style={[AuthStylesGlobal.input, AuthStylesGlobal.customW50]}
                 placeholder="DUI"
                 placeholderTextColor="gray"
+                onChangeText={text => setDUI(text)}
               />
               <TextInput
                 style={[AuthStylesGlobal.input, AuthStylesGlobal.customW50]}
                 placeholder="Telefono"
                 placeholderTextColor="gray"
+                onChangeText={text => setPhone(text)}
               />
             </View>
             <TextInput
               style={[AuthStylesGlobal.input, AuthStylesGlobal.customW91]}
               placeholder="Correo electrónico"
               placeholderTextColor="gray"
+              onChangeText={text => setEmail(text)}
             />
             <View style={AuthStylesGlobal.buttonView}>
               <CustomButton 
