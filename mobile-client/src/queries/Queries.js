858f630bc194be4c5ac196c13d51d67acfaddc70
RealAlvinJS <alvinjosuemelendez@gmail.com>
2023-07-16 13:53:55 -0600

    error de masculino-femenino

--- mobile-client/src/queries/Queries.js
+++ mobile-client/src/queries/Queries.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //! Localhost Direction of the server.
-const localhost = '192.168.1.6';
+const localhost = '192.168.0.8';
 
 //\\ const of headers
 const headers_public = { headers: {'Content-Type': 'application/json'} }
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -276,7 +276,7 @@ const register_patients = async (req, res, next) => {
     const {Email, First_Names, Last_Names, Blood_Type, Gender, Weight, Height, Selected_Date} = req.body;
 
     // CHECK EMPTY VALUES
-    if (!Email || !First_Names || !Last_Names || !Blood_Type || !Gender || !Weight || !Height || !Selected_Date) {
+    if (!Email || !First_Names || !Last_Names || !Blood_Type || Gender == undefined || !Weight || !Height || !Selected_Date) {
       return res.status(500).json({success: false, message: 'Valores vacios'});
     }
 
@@ -313,13 +313,10 @@ const register_patients = async (req, res, next) => {
     const storageRef = ref(storage, `perfil_photos/default.png`);
     const P_F = await getDownloadURL(storageRef);
 
-    console.log({First_Names, Last_Names, Birthdate: BD, Age, Gender, Blood_Type, Weight, Height, Responsible_id: responsible[0].id, Patient_Code});
-
     await pool.query('INSERT INTO patient SET ?', {First_Names, Last_Names, Birthdate: BD, Age, Gender, Blood_Type, Weight, Height, Responsible_id: responsible[0].id, Patient_Code, Medical_History_Code: null, Profile_Photo_Url: P_F, Profile_Photo_Name: null});
 
     return res.status(200).json({success: true});
   } catch (error) {
-    // console.log(error);
     return res.status(500).json({error});
   }
 }
