5e880f3890a87209d638dbd9e228b14b2d3f1ba3
Javithor360 <39425932+Javithor360@users.noreply.github.com>
2023-07-21 15:59:35 -0600

    [DESKTOP] Avance leve en citas (bugfix)
    Parte 3/?

--- desktop-client/app/src/pages/Patients/AttendMedicalAppoinment.jsx
+++ desktop-client/app/src/pages/Patients/AttendMedicalAppoinment.jsx
@@ -5,7 +5,7 @@ import parser from "html-react-parser";
 import { FaUserAlt } from "react-icons/fa";
 import { MdSaveAs } from "react-icons/md";
 
-import Modal from '../../components/Modal.jsx'
+import Modal from "../../components/Modal.jsx";
 
 import {
   EditMedicalPrescription,
@@ -30,8 +30,14 @@ export const MedicalAppoinment = () => {
     setActive(!active);
   };
 
-  const handleData = async (e) => {
-
+  const handleClick = (e) => {
+    e.preventDefault();
+    try {
+      toggle();
+      console.log("The link was clicked.");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -40,7 +46,14 @@ export const MedicalAppoinment = () => {
         <p className="text-[1.8rem] text-[#707070] mt-[.6rem] ml-7">
           Atendiendo Paciente:{" "}
         </p>
-        <button className="justify-end self-center">Finalizar consulta</button>
+        <button
+          className="justify-end self-center"
+          onClick={() => {
+            toggle();
+          }}
+        >
+          Finalizar consulta
+        </button>
       </div>
       <div className="inline-flex justify-center items-center gap-3 mt-[.5rem] ml-7">
         <FaUserAlt />
@@ -131,7 +144,7 @@ export const MedicalAppoinment = () => {
             IRREVERSIBLE ¿ESTÁS SEGURO QUE QUIERES CONTINUAR?
             <button
               className="flex items-center justify-center border-2 border-[#707070] bg-[#A375FF] text-[#FFFFFF] gap-2 w-[7rem] h-[3rem] rounded-lg ml-7 mb-9"
-              onClick={() => {handleData; toggle()}}
+              onClick={handleClick}
             >
               <MdSaveAs />
               Guardar y confirmar
