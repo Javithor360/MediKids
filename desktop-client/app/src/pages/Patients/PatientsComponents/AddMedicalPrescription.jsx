6e8ecd2fa0adda8d7ba06197a0508ff82e44e4ae
Javithor360 <39425932+Javithor360@users.noreply.github.com>
2023-08-18 16:01:05 -0600

    [DESKTOP] Avances en asignación de recetas
    - Ya se guardan en un estado los valores de cada input de "Agregar medicina" (falta ajustarlo al useState)

--- desktop-client/app/src/pages/Patients/PatientsComponents/AddMedicalPrescription.jsx
+++ desktop-client/app/src/pages/Patients/PatientsComponents/AddMedicalPrescription.jsx
@@ -1,15 +1,24 @@
 import { isActive } from "@tiptap/react";
 import React, { useEffect, useState } from "react";
 
-export const   AddMedicalPrescription = ({ setNewMedicalPrescriptionEntry }) => {
+export const AddMedicalPrescription = ({ setNewMedicalPrescriptionEntry }) => {
   const [selectedOption, setSelectedOption] = useState(false);
   const [medicines, setMedicines] = useState([
     {
       id: 0,
-      component: <MedicinesLayout key={0} selectedOption={selectedOption} />,
+      isActive: true,
+      formData: {
+        add_medicine_name: "",
+        add_instructions: "",
+        add_description: "",
+        add_dose: "",
+        add_time_dose: "",
+        add_starting_dose_date_: "",
+        add_finishing_dose_date_: "",
+      },
     },
   ]);
-  
+
   useEffect(() => {
     const updateMedicineComponents = medicines.map((m) => ({
       ...m,
@@ -21,23 +30,56 @@ export const   AddMedicalPrescription = ({ setNewMedicalPrescriptionEntry }) => 
   const handleAddComponent = async () => {
     const newMedicine = {
       id: medicines.length,
-      component: (
-        <MedicinesLayout key={medicines.length} isActive={selectedOption} />
-      ),
+      isActive: true,
+      formData: {
+        add_medicine_name: "",
+        add_instructions: "",
+        add_description: "",
+        add_dose: "",
+        add_time_dose: "",
+        add_starting_dose_date_: "",
+        add_finishing_dose_date_: "",
+      },
     };
     setMedicines([...medicines, newMedicine]);
   };
 
   const handleRemoveLastMedicine = () => {
-    if(medicines.length > 1) {
+    if (medicines.length > 1) {
       const deleteMedicines = medicines.slice(0, medicines.length - 1);
       setMedicines(deleteMedicines);
     }
-  }
+  };
+
+  const handleInputChange = (medicine_id, input_name, value) => {
+    const updatedMedicines = medicines.map((m) => {
+      if (m.id === medicine_id) {
+        return {
+          ...m,
+          formData: {
+            ...m.formData,
+            [input_name]: value,
+          },
+        };
+      }
+      return m;
+    });
+
+    setMedicines(updatedMedicines);
+  };
 
   const handleChange = async () => {
     setSelectedOption(!selectedOption);
+    const toggleMedicines = medicines.map((m) => ({
+      ...m,
+      isActive: !!m.isActive,
+    }));
+    setMedicines(toggleMedicines);
   };
+
+  useEffect(() => {
+    console.log(medicines)
+  }, [medicines])
 
   return (
     <div className="bg-cyan-100">
@@ -68,14 +110,21 @@ export const   AddMedicalPrescription = ({ setNewMedicalPrescriptionEntry }) => 
           </label>
         </form>
       </div>
-      {medicines.map((m) => {
-        return <div key={m.id}>{m.component}</div>;
-      })}
-      <div className={`${selectedOption === false ? `bg-gray-200` : ``} mt-2 inline-flex items-center gap-2`}>
+      {medicines.map((m) => (
+        <MedicinesLayout key={m.id} isActive={selectedOption} m={m} onInputChange={handleInputChange} />
+      ))}
+      <div
+        className={`${
+          selectedOption === false ? `bg-gray-200` : ``
+        } mt-2 inline-flex items-center gap-2`}
+      >
         <button onClick={handleAddComponent} disabled={!selectedOption}>
           Agregar nuevo medicamento
         </button>
-        <button onClick={handleRemoveLastMedicine} disabled={!selectedOption && medicines.length > 1}>
+        <button
+          onClick={handleRemoveLastMedicine}
+          disabled={!selectedOption && medicines.length > 1}
+        >
           Eliminar último medicamento
         </button>
       </div>
@@ -83,7 +132,12 @@ export const   AddMedicalPrescription = ({ setNewMedicalPrescriptionEntry }) => 
   );
 };
 
-const MedicinesLayout = ({ isActive }) => {
+const MedicinesLayout = ({ isActive, m, onInputChange }) => {
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    onInputChange(m.id, name, value);
+  };
+
   return (
     <div>
       <form
@@ -97,6 +151,8 @@ const MedicinesLayout = ({ isActive }) => {
             type="text"
             name="add_medicine_name"
             id="add_medicine_name"
+            value={m.formData.add_medicine_name}
+            onChange={handleInput}
           />
         </label>
         <label>
@@ -106,6 +162,8 @@ const MedicinesLayout = ({ isActive }) => {
             type="text"
             name="add_instructions"
             id="add_instructions"
+            value={m.formData.add_instructions}
+            onChange={handleInput}
           />
         </label>
         <label>
@@ -115,6 +173,8 @@ const MedicinesLayout = ({ isActive }) => {
             type="text"
             name="add_description"
             id="add_description"
+            value={m.formData.add_description}
+            onChange={handleInput}
           />
         </label>
         <label>
@@ -124,6 +184,8 @@ const MedicinesLayout = ({ isActive }) => {
             type="text"
             name="add_dose"
             id="add_dose"
+            value={m.formData.add_dose}
+            onChange={handleInput}
           />
         </label>
         <label>
@@ -133,6 +195,8 @@ const MedicinesLayout = ({ isActive }) => {
             type="text"
             name="add_time_dose"
             id="add_time_dose"
+            value={m.formData.add_time_dose}
+            onChange={handleInput}
           />
         </label>
         <label>
@@ -142,6 +206,8 @@ const MedicinesLayout = ({ isActive }) => {
             type="date"
             name="add_starting_dose_date"
             id="add_starting_dose_date"
+            value={m.formData.add_starting_dose_date}
+            onChange={handleInput}
           />
         </label>
         <label>
@@ -151,6 +217,8 @@ const MedicinesLayout = ({ isActive }) => {
             type="date"
             name="add_finishing_dose_date"
             id="add_finishing_dose_date"
+            value={m.formData.add_finishing_dose_date}
+            onChange={handleInput}
           />
         </label>
       </form>
