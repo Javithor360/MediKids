b006f9e909eff718f1adbeb0add3e8158410ef2c
Javithor360 <luisernestomr1503@gmail.com>
2023-09-05 09:03:13 -0600

    [DESKTOP] Sistema de traducción
    - Instalación de módulos
    
    Co-Authored-By: Javi <39425932+Javithor360@users.noreply.github.com>

--- desktop-client/app/src/components/index.js
+++ desktop-client/app/src/components/index.js
@@ -1,3 +1,4 @@
 export { Layout } from "./Layout.jsx";
 export { Sidebar } from "./Sidebar.jsx";
 export { Breadcrumbs } from "./Breadcrumbs.jsx";
+export { LanguageSwitcher } from "./LanguageSwitcher.jsx";
